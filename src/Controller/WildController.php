5641da57651dafbd3f5fb4c37fa786eff83e800e
TEGNY Thomas <thomas.tegny06@gmail.com>
2020-01-10 10:49:07 +0100

    Add routing with Regex to display show

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -1,19 +1,33 @@
 <?php
 namespace App\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+/**
+ * @Route("/wild", name="wild_")
+ */
 class WildController extends AbstractController
 {
-	    /**
-     * @Route("/wild", name="wild_index")
-    */
-    public function index() :Response
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(): Response
     {
-			return $this->render('wild/index.html.twig', [
-				'website' => 'Wild Séries',
-]);
+        return $this->render('wild/index.html.twig');
+    }
+    /**
+     * @Route("/show/{slug}", requirements={"slug"="^[a-z0-9-]*$"}, name="show")
+     */
+    public function show($slug): Response
+    {
+        $titre = empty($slug)
+            ? "Aucune série sélectionnée, veuillez choisir une série"
+            : ucwords(str_replace("-", " ", $slug));
+        return $this->render(
+            'wild/show.html.twig',
+            array(
+                'titre' => $titre
+            )
+        );
     }
 }
